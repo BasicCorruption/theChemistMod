219b93164bb6b6ac495521060b58a47756b00875
BasicCorruption <saytleader@outlook.com>
2022-06-23 10:59:31 -0400

    first item

--- src/main/java/io/github/basiccorruption/tcm/item/ModItems.java
+++ src/main/java/io/github/basiccorruption/tcm/item/ModItems.java
@@ -1,12 +1,18 @@
 package io.github.basiccorruption.tcm.item;
 
 import io.github.basiccorruption.tcm.TheChemistMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    private static Item registeritem(String name, Item item) {
+    // TODO: First we will add all the elements that are metals. Check trello for list of metals.
+
+    public static final Item RAW_LITHIUM = registerItem("raw_lithium", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TheChemistMod.MOD_ID, name), item);
     }
     public static void registerModItems() {
