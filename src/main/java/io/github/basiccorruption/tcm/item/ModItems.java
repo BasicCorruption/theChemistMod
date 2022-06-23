02ca06f24638a494a7f5ec465851b1912279f97c
BasicCorruption <saytleader@outlook.com>
2022-06-23 11:27:47 -0400

    now i need to figure out how to add blocks

--- src/main/java/io/github/basiccorruption/tcm/item/ModItems.java
+++ src/main/java/io/github/basiccorruption/tcm/item/ModItems.java
@@ -12,6 +12,7 @@ public class ModItems {
 
     public static final Item RAW_LITHIUM = registerItem("raw_lithium", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item LITHIUM_INGOT = registerItem("lithium_ingot", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item LITHIUM_NUGGET = registerItem("lithium_nugget", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TheChemistMod.MOD_ID, name), item);
