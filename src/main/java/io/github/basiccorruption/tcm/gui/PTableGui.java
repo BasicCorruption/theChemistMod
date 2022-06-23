3e77114e37124cab0aeeeb0d240318ef6e6beab0
BasicCorruption <saytleader@outlook.com>
2022-06-23 16:48:22 -0400

    probably broke it even more

--- src/main/java/io/github/basiccorruption/tcm/gui/PTableGui.java
+++ src/main/java/io/github/basiccorruption/tcm/gui/PTableGui.java
@@ -2,11 +2,15 @@ package io.github.basiccorruption.tcm.gui;
 
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WSprite;
+import net.minecraft.util.Identifier;
 
 public class PTableGui extends LightweightGuiDescription {
     public PTableGui() {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
         root.setSize(1377, 764);
+        // add the image to the root panel
+        root.add(new WSprite(new Identifier("tcm:gui/periodic_table.png")), 0, 0);
     }
 }
